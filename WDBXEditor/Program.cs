047ca1d77bef9181b993d6d9dd3d45d98c1909a3
Abdulrahman Alkhamees <aaalkhamees@outlook.com>
2024-01-03 16:03:57 +0300

    Update Program.cs

--- WDBXEditor/Program.cs
+++ WDBXEditor/Program.cs
@@ -1,14 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Reflection;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using WDBXEditor.ConsoleHandler;
-using Button = System.Windows.Forms.Button;
 
 namespace WDBXEditor
 {
