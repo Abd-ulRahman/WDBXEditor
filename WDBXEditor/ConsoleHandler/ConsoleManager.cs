d9652b49cf833b48177a373d18585d7658ca6aec
Abdulrahman Alkhamees <aaalkhamees@outlook.com>
2024-01-03 14:52:18 +0300

    Update ConsoleManager.cs

--- WDBXEditor/ConsoleHandler/ConsoleManager.cs
+++ WDBXEditor/ConsoleHandler/ConsoleManager.cs
@@ -53,40 +53,6 @@ namespace WDBXEditor.ConsoleHandler
                 return false;
             }
         }
-        public static string RunCommand(string arguments, bool readOutput)
-        {
-            var output = string.Empty;
-            try
-            {
-                var startInfo = new ProcessStartInfo
-                {
-                    Verb = "runas",
-                    FileName = "cmd.exe",
-                    Arguments = "/C " + arguments,
-                    WindowStyle = ProcessWindowStyle.Hidden,
-                    UseShellExecute = false,
-                    CreateNoWindow = true,
-                    RedirectStandardInput = true,
-                    RedirectStandardOutput = true,
-                    RedirectStandardError = false
-                };
-
-                var proc = Process.Start(startInfo);
-
-                if (readOutput)
-                {
-                    output = proc.StandardOutput.ReadToEnd();
-                }
-
-                proc.WaitForExit(60000);
-
-                return output;
-            }
-            catch (Exception)
-            {
-                return output;
-            }
-        }
 
         public static void LoadCommandDefinitions()
         {
