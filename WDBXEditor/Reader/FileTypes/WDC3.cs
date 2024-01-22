3cb73a32a0244a36abba65339f62a3e64e96e631
Abdulrahman Alkhamees <aaalkhamees@outlook.com>
2024-01-22 14:02:21 +0300

    Update WDC3.cs

--- WDBXEditor/Reader/FileTypes/WDC3.cs
+++ WDBXEditor/Reader/FileTypes/WDC3.cs
@@ -69,7 +69,7 @@ namespace WDBXEditor.Reader.FileTypes
                     RecordOffset = dbReader.ReadUInt16(),
                     Size = dbReader.ReadUInt16(),
                     AdditionalDataSize = dbReader.ReadUInt32(), // size of pallet / sparse values
-                    CompressionType = (CompressionType)dbReader.ReadUInt32(),
+                    CompressionType = (CompressionType)dbReader.Read(),
                     BitOffset = dbReader.ReadInt32(),
                     BitWidth = dbReader.ReadInt32(),
                     Cardinality = dbReader.ReadInt32()
